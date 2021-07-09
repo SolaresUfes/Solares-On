99e31c52185e2a11924bcc38b60294524c40506c
Lipao12 <f.nunesmai19@gmail.com>
2021-07-09 08:58:10 -0300

    Criação da função para calcular a energia ativa diária e  para calcular a potencia mínima dos arranjos fotovoltaicos
    Foi retirado a variável horasDeSolPleno. Na funcão energiaAtivaNecessariaDia() precisa, como parâmetros a potência utilizada em corrente alternada e contínua(talvez precise do rendimento do inversor também), o rendimento da bateria é um valor médio pré-definido, 0.9 . Na função potenciaMinimaArranjoFotovoltaico(), os parâmetros são o vetorCidade e o L (potência ativa diária), essa função deve encontrar a potência máxima que é utilizada em um mês.

--- app/src/main/java/com/solares/calculadorasolar/classes/CalculadoraOffGrid.java
+++ app/src/main/java/com/solares/calculadorasolar/classes/CalculadoraOffGrid.java
@@ -19,8 +19,10 @@ public class CalculadoraOffGrid {
     double potenciaUtilizadaDiariaCA=0;
     double potenciaUtilizadaDiariaTotal=0;
     double horasDeSolPleno=0;
-    double energiaNecessariaDia=0;
+    double energiaAtivaDia=0;
+    double minPotencia=0;
     double area=0;
+    String[] inversor;
 
     float areaAlvo;
     int idModuloEscolhido;
@@ -40,6 +42,10 @@ public class CalculadoraOffGrid {
     }
     public void setVetorQntEquipamentosCA(int[] vetorQntEquipamentos){this.vetorQntEquipamentosCA = vetorQntEquipamentos;}
     public void setVetorQntEquipamentosCC(int[] vetorQntEquipamentos){this.vetorQntEquipamentosCC = vetorQntEquipamentos;}
+    public void setAreaAlvo(float AreaAlvo) { this.areaAlvo = AreaAlvo; }
+    public void setIdModuloEscolhido(int idModuloEscolhido){this.idModuloEscolhido = idModuloEscolhido;}
+
+
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////        Função Principal       /////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -49,28 +55,38 @@ public class CalculadoraOffGrid {
         try {
             CalculadoraOnGrid calculadoraOnGrid = new CalculadoraOnGrid();  // talvez mudar isso para uma funcao principal, que essa irá herdar
 
-            // Calcular a Hora de Sol Pleno do Local
-            this.horasDeSolPleno = calculadoraOnGrid.MeanSolarHour(vetorCidade);
-
             // Calcular a Demanda de Energia Total
             // - corrente contínua
             this.potenciaUtilizadaDiariaCC = demandaEnergeticaDiaria(matrizEquipamentosCC, vetorQntEquipamentosCC);
             // - corrente alternada
             this.potenciaUtilizadaDiariaCA = demandaEnergeticaDiaria(matrizEquipamentosCA, vetorQntEquipamentosCA);
-            //this.potenciaUtilizadaDiariaTotal = demandaEnergeticaDiaria(matrizEquipamentos, vetorQntEquipamentos); // talvez nao precise dos parametros
+            //this.potenciaUtilizadaDiariaTotal = demandaEnergeticaDiaria(matrizEquipamentos, vetorQntEquipamentos);
             this.potenciaUtilizadaDiariaTotal = this.potenciaUtilizadaDiariaCC + this.potenciaUtilizadaDiariaCA;
 
             // Calcular a Energia Ativa Necessária Diariamente
-            //this.energiaNecessariaDia = 1;
+            this.energiaAtivaDia = energiaAtivaNecessariaDia(this.potenciaUtilizadaDiariaCA, this.potenciaUtilizadaDiariaCC);
 
-            //Definindo as placas
+            // Calcular Potência Mínima do Arranjo Fotovoltaico
+            this.minPotencia = potenciaMinimaArranjoFotovoltaico(vetorCidade,this.energiaAtivaDia);
+
+            // Definindo as Placas
             is = MyContext.getResources().openRawResource(R.raw.banco_paineis);
-            placaEscolhida = CSVRead.DefineSolarPanel(is, potenciaUtilizadaDiariaTotal, this.areaAlvo, this.idModuloEscolhido);
+            placaEscolhida = CSVRead.DefineSolarPanel(is, this.minPotencia, this.areaAlvo, this.idModuloEscolhido);
             area = calculadoraOnGrid.DefineArea(placaEscolhida);
+
+            // Definindo os Inversores
+            is = MyContext.getResources().openRawResource(R.raw.banco_inversores);
+            inversor = CSVRead.DefineInvertor(is, placaEscolhida, this.idInversorEscolhido);
             
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public static double MeanSolarHour(String cityVec){
+        double solarHour = 0.0;
+        solarHour = Double.parseDouble(cityVec)/1000.0;
+        return solarHour;
     }
 
     public static double demandaEnergeticaDiaria(ArrayList<ArrayList<String>> matrizEquipamentos, int[] vetorQntEquipamentos){
@@ -87,5 +103,33 @@ public class CalculadoraOffGrid {
             totalPower = eachElementPower + totalPower;
         }
         return totalPower / 0.9;
+    }
+
+    public static double energiaAtivaNecessariaDia(double Lcc, double Lca){
+        double L=0;
+        double rendimentoBat=0.9;
+        double rendimentoInv=0;
+        L = Lcc/rendimentoBat + Lca/(rendimentoBat * rendimentoInv);
+        return L;
+    }
+
+    public static double potenciaMinimaArranjoFotovoltaico(String[] vetorCidade, double L){
+        double Lmes=0;
+        double HSPi=0;
+        double Pi=0;
+        double Pmax=0;
+        double Red1 = 0.75;
+        double Red2 = 0.9;
+        for (int i=0; i<12; i++){
+            if(i==1) Lmes = L * 28;
+            else if(i%2==0) Lmes = L*31;
+            else Lmes = L * 30;
+
+            HSPi = MeanSolarHour(vetorCidade[i]);
+            Pi = Lmes * (HSPi * Red1 * Red2);
+
+            if(Pi > Pmax) Pmax = Pi;
+        }
+        return Pmax;
     }
 }

5f4f338d68f20028d3e44eca2e1095182cb5782f
Lipao12 <f.nunesmai19@gmail.com>
2021-08-02 15:39:32 -0300

    Criação da activity para pedir o consumo energético

---
+++ app/src/main/java/com/solares/calculadorasolar/activity/PedirConsumoEnergetico.java
@@ -0,0 +1,16 @@
+package com.solares.calculadorasolar.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.solares.calculadorasolar.R;
+
+public class PedirConsumoEnergetico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.solares.calculadorasolar.R.layout.activity_pedir_consumo_energetico);
+    }
+}
